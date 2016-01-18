555d797e2fa11120d30cae3ac140ce283d57e619
Iker Vazquez <ikervr@gmail.com>
2016-01-18 21:04:05 +0100

    Refractor middleware

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+var middleware = require('./middleware');
+
+app.use(middleware.logger);
+
 app.get('/', function(req, res){
     res.send('Hey boy, hey girl');    
 });
