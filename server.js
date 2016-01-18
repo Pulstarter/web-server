2361e63f21c9867b9a6c452d1363bd6fb004d70c
Iker Vazquez <ikervr@gmail.com>
2016-01-18 20:50:30 +0100

    Change title

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('Hello madafaka');    
+    res.send('Hey boy, hey girl');    
 });
 
 app.use('/public', express.static(__dirname + '/public'));
